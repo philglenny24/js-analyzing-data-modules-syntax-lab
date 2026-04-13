2181de97acd29f08259b4a374e07bdf066f5d91a
philglenny24 <glennytronephilip@gmail.com>
2026-04-13 22:26:22 +0300

    add function that merges all usernames into one

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+require('datejs');
 
+function combineUsers(...args) {
+  
+  const combinedObject = {
+    users: []
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    const currentArray = args[i];
+
+    combinedObject.users = [...combinedObject.users, ...currentArray];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+const legacyUsers = ['@admin', '@mod_77'];
+const newUsers = ['@superstar', '@coding_wiz'];
+const temporaryUsers = ['@guest_01'];
+
+const result = combineUsers(legacyUsers, newUsers, temporaryUsers);
+console.log(result);
 
 
 module.exports = {
